09dfc600ca71211f035d81db09c256d70dd52dd1
chaeeun037 <kongkce@kong-tech.com>
2019-01-21 16:45:38 +0900

    test

---
+++ .vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'VuePress test project',
+  description: ':) test',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Kong Check', link: 'http://check.kong-tech.com' },
+    ],
+    sidebar: 'auto',
+    lastUpdated: 'Last Updated'
+  }
+}
---
+++ guide/README.md
@@ -0,0 +1,7 @@
+# Guide
+
+가이드 내용입니다.
+
+## Other Considerations
+
+뜌하하
